f2ae034f06ec943bdd18ae21000d12c851b04d5a
Akshay Chhikara <akshay.chhikara@vinsol.com>
2015-01-05 14:15:18 +0530

    CS-1 1) respond_to removed from companies_controller 2) added respective css to respective pages

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,15 +5,11 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new(company_params)
-    respond_to do |format|
-      if @company.save
-        format.html { redirect_to root_url, notice: 'You have been successfully registered.' }
-      else
-        format.html do
-          flash.now[:alert] = @company.errors.full_messages.to_a.to_sentence
-          render :new
-        end
-      end
+    if @company.save
+      redirect_to root_url, notice: 'You have been successfully registered.'
+    else
+      flash.now[:alert] = @company.errors.full_messages.to_a.to_sentence
+      render :new
     end
   end
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( company.css application_index.css twitter-bootstrap-static/bootstrap.css)
