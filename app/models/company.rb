c7802f6fc79dbd4c791eff3de3f7f012559acf74
Akshay Chhikara <akshay.chhikara@vinsol.com>
2015-01-06 19:05:06 +0530

    CS-1 added format for subdomain

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,4 @@
 class Company < ActiveRecord::Base
   validates :name, :subdomain, presence: true
-  validates :subdomain, uniqueness: true
+  validates :subdomain, uniqueness: true, format: { with: /\A[a-z\d]+([-_][a-z\d]+)*\Z/i }
 end
