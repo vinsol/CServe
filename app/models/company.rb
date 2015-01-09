2bc7389698395fd607329cff442c7f56ccd39a81
Akshay Chhikara <akshay.chhikara@vinsol.com>
2015-01-09 12:29:13 +0530

    fixed Error messages in registration

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ActiveRecord::Base
-  validates :name, :subdomain, presence: true
-  validates :subdomain, uniqueness: true, format: { with: /\A[a-z\d]+([-_][a-z\d]+)*\Z/i }
-  validates :subdomain, uniqueness: true
+
+  validates :name, presence: true
+  validates :subdomain, presence: true, uniqueness: true, format: { with: /\A[a-z\d]+([-_][a-z\d]+)*\Z/i }
   has_many :admins, dependent: :destroy
+
 end
