19e9f62d7fd21e82503a94b51e49c4f8523d0bf3
Akshay Chhikara <akshay.chhikara@vinsol.com>
2015-01-12 21:01:02 +0530

    Sprint-1 password token validity method added

--- app/controllers/admins/passwords_controller.rb
+++ app/controllers/admins/passwords_controller.rb
@@ -1,10 +1,13 @@
 class Admins::PasswordsController < Devise::PasswordsController
 
-  def edit
-    self.resource = resource_class.reset_password_by_token(params)
-    if resource.errors.empty?
-      super
-    else
+  before_filter :check_validity_of_token?, only: [:edit]
+
+  private
+  def check_validity_of_token?
+    original_token       = params[:reset_password_token]
+    reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
+    recoverable = resource_class.find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
+    if recoverable.invalid?
       flash[:alert] = 'Link expired get new link again'
       redirect_to new_admin_password_path
     end
