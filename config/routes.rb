e7aaafd0c15a72ce08fefd0325e6045361f7731d
Akshay Chhikara <akshay.chhikara@vinsol.com>
2015-01-17 17:21:07 +0530

    CS-6 route changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,15 @@ Rails.application.routes.draw do
     :sessions => 'admins/sessions',
     :passwords => 'admins/passwords'
   }
-  resources :companies, only: [:new, :create]
   resources :admins, only: [:index]
-  # resources :admins do
-    # get 'edit', on: :collection
-    # patch 'edit', on: :collection
-    # get 'add_admin', on: :new
-    # post 'add_admin', on: :create
-    # post 'change_state', on: :new
-  # end
-  get '/sign_up', to: 'companies#new'
-  get '/add_admin', to: 'admins#new'
-  post '/add_admin', to: 'admins#create'
+  controller :companies do
+    get 'sign_up' => :new
+    post 'sign_up' => :create
+  end
+  controller :admins do
+    get 'add_admin' => :new
+    post 'add_admin' => :create
+  end
   post '/change_state/:id', to: 'admins#change_state', as: 'change_state'
   get '/edit/:id', to: 'admins#edit', as: 'edit'
   patch '/edit/:id', to: 'admins#update'
