b0f1b92a6dc6679f9ef48e09843b39ca33b004e9
Akshay chhikara <akshay.chhikara@gmail.com>
2015-01-27 18:14:29 +0530

    Refactoring in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
 
+  before_action :load_company
+  before_action :load_ticket
+  before_action :ensure_correct_company
+
   def create
-    @ticket = Ticket.find_by(id: params[:ticket_id])
     @comment = @ticket.comments.build(comment_params)
     @comment.set_commenter_email(current_admin, @ticket)
     if @comment.save
@@ -14,6 +17,20 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:text)
+    end
+
+    def load_ticket
+      @ticket = Ticket.find_by(id: params[:ticket_id])
+      redirect_to root_path, alert: 'Ticket not found.' unless @ticket
+    end
+
+    def load_company
+      @company = Company.find_by(name: request.subdomain)
+      redirect_to root_path, alert: 'Company not found.' unless @company
+    end
+
+    def ensure_correct_company
+      redirect_to root_path, alert: 'Incorrect Company' unless @company.id == @ticket.company_id
     end
 
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,8 +2,9 @@ class TicketsController < ApplicationController
 
   layout 'admins', only: [:index, :show, :new]
 
-  before_action :authenticate_admin!, only: [:index]
-  before_action :check_subdomain?, only: [:new]
+  before_action :authenticate_admin!, only: :index
+  before_action :check_subdomain?, only: :new
+  before_action :load_company, only: :create
 
   def index
     @tickets = Ticket.where(company_id: current_admin.company_id).order(:updated_at).page(params[:page])
@@ -15,7 +16,6 @@ class TicketsController < ApplicationController
   end
 
   def create
-    @company = Company.where(name: request.subdomain).first
     @ticket = @company.tickets.build(ticket_params)
     if @ticket.save
       redirect_to new_ticket_path, notice: 'Feedback Successfully Submitted'
@@ -28,13 +28,18 @@ class TicketsController < ApplicationController
   def show
     @ticket =  Ticket.find_by(id: params[:id])
     @comments = @ticket.comments
-    @comment = Comment.new
+    @comment = @ticket.comments.new
   end
 
   private
 
     def ticket_params
       params.require(:ticket).permit(:email, :description, :subject, attachments_attributes: [:document])
+    end
+
+    def load_company
+      @company = Company.find_by(name: request.subdomain)
+      redirect_to root_path, alert: 'Company not found.' unless @company
     end
 
 end
