7a483ef1b18ed960a78f109e8f2a00a6fbbdcc5f
Akshay chhikara <akshay.chhikara@gmail.com>
2015-01-28 15:44:48 +0530

    QA fixes

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,6 @@
 class AdminMailer < ApplicationMailer
 
   def set_password_instructions(admin, token)
-  	debugger
     @admin, @token = admin, token
     mail(to: @admin.unconfirmed_email, subject: 'Set Password for your account')
   end
